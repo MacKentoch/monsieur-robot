b22d03175b93a19734777647e158e142bff393c1
MacKentoch <erwan.datin@gmail.com>
2017-12-13 18:34:42 +0100

    assets icons

--- pages/_document.js
+++ pages/_document.js
@@ -93,9 +93,9 @@ class RootDocument extends Document<Props, State> {
 
           <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png" />
-          <meta name="theme-color" content="#1967be" />
+          <meta name="theme-color" content="#00897b" />
 
-          <link rel="mask-icon" href="static/safari-pinned-tab.svg" color="#1967be" />
+          <link rel="mask-icon" href="static/safari-pinned-tab.svg" color="#00897b" />
           <meta name="apple-mobile-web-app-title" content="M. robot" />
           <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png" />
           <link rel="apple-touch-startup-image" href="static/apple-touch-icon.png" />
