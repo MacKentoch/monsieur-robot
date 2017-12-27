69fac3dd9a342bb6fe4f531deced9b3b84ac025b
MacKentoch <erwan.datin@gmail.com>
2017-12-27 06:52:41 +0100

    newsletter

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -9,6 +9,7 @@ import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import Input, { InputLabel } from 'material-ui/Input';
 import { FormControl, FormHelperText } from 'material-ui/Form';
+import Grid from 'material-ui/Grid';
 import { withStyles } from 'material-ui/styles';
 import withRoot from '../HOC/withRoot';
 import Layout from '../components/layout/Layout';
@@ -49,12 +50,42 @@ type State = {
 // #region styles
 const styles = theme => ({
   root: {},
+  contentContainer: {
+    display: 'flex',
+    flex: '1 0 auto',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '30px',
+  },
   nameContainer: {
     display: 'flex',
+    flex: '1 0 auto',
+    flexDirection: 'row',
+    justifyContent: 'center',
     flexWrap: 'wrap',
+    maxWidth: '1200px',
   },
   formInput: {
     margin: theme.spacing.unit,
+    width: '390px',
+  },
+  formInputEmail: {
+    margin: theme.spacing.unit,
+    width: '800px',
+  },
+  formInputInfoText: {
+    width: '800px',
+    marginTop: '20px',
+    marginBottom: '20px',
+  },
+  joinButtonContainer: {
+    display: 'flex',
+    flex: '1 0 auto',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    maxWidth: '1200px',
+    marginTop: '30px',
   },
 });
 // #endregion
@@ -85,35 +116,54 @@ class NewsLetter extends PureComponent<Props, State> {
         <Typography type="display1" gutterBottom>
           Signup for the Fides Project news
         </Typography>
+        <div className={classes.contentContainer}>
+          <div className={classes.nameContainer}>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="name-simple">First Name</InputLabel>
+              <Input
+                id="firstname"
+                value={firstname}
+                className={classes.formInput}
+                onChange={this.handlesInpuntChange('fistname')}
+                fullWidth
+              />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="name-simple">Last Name</InputLabel>
+              <Input
+                id="lastname"
+                value={lastname}
+                className={classes.formInput}
+                onChange={this.handlesInpuntChange('lastname')}
+                fullWidth
+              />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="name-simple">Email</InputLabel>
+              <Input
+                id="email"
+                value={email}
+                className={classes.formInputEmail}
+                onChange={this.handlesInpuntChange('email')}
+                fullWidth
+              />
+            </FormControl>
+            <div className={classes.formInputInfoText}>
+              <Typography type="body1" gutterBottom>
+                The information you provide here will only be used to send you
+                updates and opportunities from Tor. You can unsubscribe at any
+                time. We will not publish, sell, trade, share, or rent any
+                information about you.
+              </Typography>
+            </div>
+          </div>
 
-        <div className={classes.nameContainer}>
-          <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="name-simple">First Name</InputLabel>
-            <Input
-              id="firstname"
-              value={firstname}
-              onChange={this.handlesInpuntChange('fistname')}
-            />
-          </FormControl>
-          <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="name-simple">Last Name</InputLabel>
-            <Input
-              id="lastname"
-              value={lastname}
-              onChange={this.handlesInpuntChange('fistname')}
-            />
-          </FormControl>
+          <div className={classes.joinButtonContainer}>
+            <Button raised color="primary" onClick={this.handleClick}>
+              Join
+            </Button>
+          </div>
         </div>
-
-        <Typography type="body1" gutterBottom>
-          The information you provide here will only be used to send you updates
-          and opportunities from Tor. You can unsubscribe at any time. We will
-          not publish, sell, trade, share, or rent any information about you.
-        </Typography>
-
-        <Button raised color="primary" onClick={this.handleClick}>
-          Go back Home
-        </Button>
       </Layout>
     );
   }
