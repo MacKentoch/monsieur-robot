9960fdd5eb961207f88146dab57d226256732c27
MacKentoch <erwan.datin@gmail.com>
2017-12-27 06:35:47 +0100

    newletter

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -1,12 +1,14 @@
 // @flow
 
 // #region imports
-import React, { PureComponent } from 'react';
+import React, { PureComponent, SyntheticEvent } from 'react';
 import Router from 'next/router';
 import { compose, bindActionCreators } from 'redux';
 import withRedux from 'next-redux-wrapper';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
+import Input, { InputLabel } from 'material-ui/Input';
+import { FormControl, FormHelperText } from 'material-ui/Form';
 import { withStyles } from 'material-ui/styles';
 import withRoot from '../HOC/withRoot';
 import Layout from '../components/layout/Layout';
@@ -35,13 +37,25 @@ type Props = {
 };
 
 type State = {
+  // input fields
+  firstname: string,
+  lastname: string,
+  email: string,
+
   ...any,
 };
 // #endregion
 
 // #region styles
 const styles = theme => ({
-  // to add
+  root: {},
+  nameContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formInput: {
+    margin: theme.spacing.unit,
+  },
 });
 // #endregion
 
@@ -52,25 +66,65 @@ class NewsLetter extends PureComponent<Props, State> {
   }
   // #endregion
 
+  // #region state initialization
+  state = {
+    // input fields
+    firstname: '',
+    lastname: '',
+    email: '',
+  };
+  // #endregion
+
   // #region component lifecycle methods
   render() {
-    const { pathname } = this.props;
+    const { pathname, classes } = this.props;
+    const { firstname, lastname, email } = this.state;
 
     return (
       <Layout pathname={pathname} navigationMenus={<NavMenus />}>
         <Typography type="display1" gutterBottom>
-          NewsLetter
+          Signup for the Fides Project news
         </Typography>
 
-        <Typography type="subheading" gutterBottom>
-          to add more content
+        <div className={classes.nameContainer}>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="name-simple">First Name</InputLabel>
+            <Input
+              id="firstname"
+              value={firstname}
+              onChange={this.handlesInpuntChange('fistname')}
+            />
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="name-simple">Last Name</InputLabel>
+            <Input
+              id="lastname"
+              value={lastname}
+              onChange={this.handlesInpuntChange('fistname')}
+            />
+          </FormControl>
+        </div>
+
+        <Typography type="body1" gutterBottom>
+          The information you provide here will only be used to send you updates
+          and opportunities from Tor. You can unsubscribe at any time. We will
+          not publish, sell, trade, share, or rent any information about you.
         </Typography>
+
         <Button raised color="primary" onClick={this.handleClick}>
           Go back Home
         </Button>
       </Layout>
     );
   }
+  // #endregion
+
+  // #region on form input change event
+  handlesInpuntChange = (field: string = '') => (event: SyntheticEvent<>) => {
+    if (event) {
+      event.preventDefault();
+    }
+  };
   // #endregion
 
   handleClick = () => {
