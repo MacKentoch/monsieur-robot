dfc7bbba09992bca7dc5125bc23d2e8f16bd5137
MacKentoch <erwan.datin@gmail.com>
2017-12-31 09:57:20 +0100

    update server config to convict system

---
+++ server/lib/nextExpress.js
@@ -0,0 +1,62 @@
+// @flow
+
+// #region imports
+const express = require('express');
+const helmet = require('helmet');
+const chalk = require('chalk');
+const bodyParser = require('body-parser');
+const RateLimit = require('express-rate-limit');
+const logger = require('morgan');
+const compress = require('compression');
+const config = require('../config');
+// #endregion
+
+const nextExpress = async app => {
+  await app.prepare();
+
+  const handle = app.getRequestHandler();
+  const server = express();
+
+  // #region add app middlewares
+  if (config.get('env') !== 'production') {
+    server.use(logger('dev'));
+  }
+
+  server.use(helmet());
+  server.use(compress());
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: true }));
+  // #endregion
+
+  if (config.get('env') === 'production') {
+    // express-rate-limit in production to limit over-repeated requests to API
+    server.use('/api/', new RateLimit(config.get('rateLimit')));
+  }
+
+  // #region handles service worker file request (NOTE: it won't work in dev mode but production only):
+  server.get('/sw.js', (req, res) =>
+    app.serveStatic(req, res, '.next/service-worker.js'),
+  );
+  // #endregion
+
+  // default request handler by next handler:
+  server.get('*', (req, res) => handle(req, res));
+
+  const port = config.get('server.port');
+  server.listen(port, err => {
+    if (err) {
+      throw err;
+    }
+    const title = chalk.bgBlue('monsieur robot');
+    const host = chalk.green(config.get('server.host'));
+    /* eslint-disable no-console */
+    console.log(`
+        ===============================================================
+        -> Server (${title}) 🏃 (running) on ${host}:${chalk.green(port)}
+        ===============================================================
+      `);
+    /* eslint-enable no-console */
+  });
+};
+
+module.exports = nextExpress;
--- server/server.js
+++ server/server.js
@@ -2,28 +2,19 @@
 /* eslint-disable no-process-env */
 
 // #region imports
-const express = require('express');
-const helmet = require('helmet');
-const chalk = require('chalk');
 const next = require('next');
-const bodyParser = require('body-parser');
-const RateLimit = require('express-rate-limit');
-const logger = require('morgan');
-const compress = require('compression');
-const appConfig = require('./config/appConfig');
+const nextExpress = require('./lib/nextExpress');
+const config = require('./config');
 // #endregion
 
 // #region variables/constants initialization
-const port = parseInt(process.env.PORT, 10) || appConfig.defaultPort;
-const ipAdress = 'localhost';
-const dev = process.env.NODE_ENV !== 'production';
+const dev = config.get('env') !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
 // #endregion
 
 // #region start next application
 try {
-  prepareNextApplication();
+  nextExpress(app);
 } catch (error) {
   /* eslint-disable no-console */
   console.log('prepareNextApplication FAILED: ', error);
@@ -31,48 +22,48 @@ try {
 }
 // #endregion
 
-async function prepareNextApplication() {
-  await app.prepare();
+// async function prepareNextApplication() {
+//   await app.prepare();
 
-  const server = express();
+//   const server = express();
 
-  // #region add app middlewares
-  if (dev) {
-    server.use(logger('dev'));
-  }
+//   // #region add app middlewares
+//   if (dev) {
+//     server.use(logger('dev'));
+//   }
 
-  server.use(helmet());
-  server.use(compress());
-  server.use(bodyParser.json());
-  server.use(bodyParser.urlencoded({ extended: true }));
-  // #endregion
+//   server.use(helmet());
+//   server.use(compress());
+//   server.use(bodyParser.json());
+//   server.use(bodyParser.urlencoded({ extended: true }));
+//   // #endregion
 
-  if (!dev) {
-    // express-rate-limit in production to limit over-repeated requests to API
-    server.use('/api/', new RateLimit(appConfig.rateLimit));
-  }
+//   if (!dev) {
+//     // express-rate-limit in production to limit over-repeated requests to API
+//     server.use('/api/', new RateLimit(appConfig.rateLimit));
+//   }
 
-  // #region handles service worker file request (NOTE: it won't work in dev mode but production only):
-  server.get('/sw.js', (req, res) =>
-    app.serveStatic(req, res, '.next/service-worker.js'),
-  );
-  // #endregion
+//   // #region handles service worker file request (NOTE: it won't work in dev mode but production only):
+//   server.get('/sw.js', (req, res) =>
+//     app.serveStatic(req, res, '.next/service-worker.js'),
+//   );
+//   // #endregion
 
-  // default request handler by next handler:
-  server.get('*', (req, res) => handle(req, res));
+//   // default request handler by next handler:
+//   server.get('*', (req, res) => handle(req, res));
 
-  server.listen(port, err => {
-    if (err) {
-      throw err;
-    }
-    const title = chalk.bgBlue('monsieur robot');
-    const ip = chalk.green(ipAdress);
-    /* eslint-disable no-console */
-    console.log(`
-        ===============================================================
-        -> Server (${title}) 🏃 (running) on ${ip}:${chalk.green(port)}
-        ===============================================================
-      `);
-    /* eslint-enable no-console */
-  });
-}
+//   server.listen(port, err => {
+//     if (err) {
+//       throw err;
+//     }
+//     const title = chalk.bgBlue('monsieur robot');
+//     const ip = chalk.green(ipAdress);
+//     /* eslint-disable no-console */
+//     console.log(`
+//         ===============================================================
+//         -> Server (${title}) 🏃 (running) on ${ip}:${chalk.green(port)}
+//         ===============================================================
+//       `);
+//     /* eslint-enable no-console */
+//   });
+// }
