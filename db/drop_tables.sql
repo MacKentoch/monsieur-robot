ab7386c7fa446772a4c05155328e226acfc3d3b8
MacKentoch <erwan.datin@gmail.com>
2018-04-30 14:35:27 +0200

    drop table

---
+++ db/drop_tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE public.blogs_translations;
+DROP TABLE public.blogs;
+DROP TABLE public.authors;
+DROP TABLE public.languages;
+DROP TABLE public.page_home;
+DROP TABLE public.newsletters;
+DROP TABLE public.newsletters_subscribers;
